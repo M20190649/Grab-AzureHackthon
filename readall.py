83368bd39bfd12b7d85afc20a8191e71a30bf351
Fabian Tan <fnever520@gmail.com>
2020-05-11 13:37:50 +0900

    Added

--- readall.py
+++ readall.py
@@ -3,18 +3,32 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import geopandas as gpd
 from shapely.geometry import Point, Polygon
+import osmnx as ox
+import networkx as nx
+
+# from collections import defaultdict
+# seen = defaultdict(int)
+# ddd = list(df['trj_id'])
+# for i in ddd:
+#     seen[i] += 1
+
+run = False
 
 def convert_time(data):
     data['realtime'] = pd.to_datetime(data['pingtimestamp'], unit = 's')
     return data
 
 def plot(df):
+    # sg = ox.graph_from_place('Singapore', network_type ='all')
+    fig, ax = plt.subplots(figsize = (8,6))
+    sg = ox.load_graphml('a.graphml')
+    ox.plot_graph(sg,show=False)
     #Using list comprehension, specify the “Longitude” column before the “Latitude” column
     geometry = [Point(xy) for xy in zip(df["rawlng"], df["rawlat"])]
     # CRS: coordinate reference system
     crs = {'init' : 'epsg:4326'}
     geo_df = gpd.GeoDataFrame(df, crs = crs, geometry = geometry)
-    fig, ax = plt.subplots(figsize = (8,6))
+    
     plt.scatter(geo_df.rawlng, geo_df.rawlat, s = 3, c='red', label = "trj_10")
     plt.xlabel("Longitude")
     plt.ylabel("Latitude")
@@ -22,20 +36,18 @@ def plot(df):
     plt.title("Singapore")
     plt.show()
 
-def main():
-    df_list = []
-    for file in glob.glob("dataset/part-*.parquet"):
-        df_ = pd.read_parquet(file, engine = 'pyarrow')
-        df_list.append(df_)
+df_list = []
+for file in glob.glob("dataset/part-*.parquet"):
+    df_ = pd.read_parquet(file, engine = 'pyarrow')
+    df_list.append(df_)
 
-    df = pd.concat(df_list, ignore_index=True)
-    print(df.shape)
-    convert_time(df)
-    df.drop(columns = ["pingtimestamp"], inplace=True)
-    df = df.sort_values(by=['trj_id', 'realtime'], ascending=True)
+df = pd.concat(df_list, ignore_index=True)
+print(df.shape)
+convert_time(df)
+df.drop(columns = ["pingtimestamp"], inplace=True)
+df = df.sort_values(by=['trj_id', 'realtime'], ascending=True)
 
-    #try
+#try
+if run():
     df = df[df["trj_id"] == "10"]
     plot(df)
-
-main()
