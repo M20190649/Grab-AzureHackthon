e829c3a4661c04f28ee67f816820f357a99906cb
Fabian Tan <fnever520@git.com>
2020-05-16 15:36:22 +0900

    added

--- main.py
+++ main.py
@@ -19,15 +19,15 @@ def convert_time(data):
 
 def plot(df):
     # sg = ox.graph_from_place('Singapore', network_type ='all')
-    fig, ax = plt.subplots(figsize = (8,6))
-    sg = ox.load_graphml('a.graphml')
-    ox.plot_graph(sg,show=False)
+    # sg = ox.load_graphml('a.graphml')
+    # ox.plot_graph(sg,show=False)
+    
     #Using list comprehension, specify the “Longitude” column before the “Latitude” column
     geometry = [Point(xy) for xy in zip(df["rawlng"], df["rawlat"])]
     # CRS: coordinate reference system
     crs = {'init' : 'epsg:4326'}
     geo_df = gpd.GeoDataFrame(df, crs = crs, geometry = geometry)
-    
+    fig, ax = plt.subplots(figsize = (8,6))
     plt.scatter(geo_df.rawlng, geo_df.rawlat, s = 3, c='red', label = "trj_10")
     plt.xlabel("Longitude")
     plt.ylabel("Latitude")
@@ -44,7 +44,7 @@ df = pd.concat(df_list, ignore_index=True)
 print(df.shape)
 convert_time(df)
 df.drop(columns = ["pingtimestamp"], inplace=True)
-df = df.sort_values(by=['trj_id', 'realtime'], ascending=True)
+df.sort_values(by=['trj_id', 'realtime'], ascending=True, inplace=True)
 
 #plot single trajectories
 data1 = df[df["trj_id"] == "10"]
